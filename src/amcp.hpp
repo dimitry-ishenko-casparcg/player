203e2d59f0a2f9b39b7e6ca439b57d46e955dd9f
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-09-28 19:27:52 -0400

    amcp: refactor

--- src/amcp.hpp
+++ src/amcp.hpp
@@ -9,7 +9,9 @@
 #define AMCP_HPP
 
 #include <asio.hpp>
+#include <stdexcept>
 #include <string>
+#include <system_error>
 
 ////////////////////////////////////////////////////////////////////////////////
 namespace amcp
@@ -20,13 +22,34 @@ class connection
     asio::ip::tcp::resolver resolver_;
     asio::ip::tcp::socket socket_;
 
-    void async_send(const std::string&);
+    void async_send(const std::string& cmd)
+    {
+        asio::async_write(socket_, asio::buffer(cmd),
+            [](asio::error_code ec, std::size_t){ if (ec) throw std::system_error{ec}; }
+        );
+    }
 
 public:
-    connection(asio::io_context&, const std::string& server);
+    connection(asio::io_context& io, const std::string& server) :
+        resolver_{io}, socket_{io}
+    {
+        auto p = server.rfind(':');
+        if (p == server.npos) throw std::invalid_argument{"Port number not specified"};
 
-    void osc_enable(asio::ip::port_type);
-    void osc_disable(asio::ip::port_type);
+        auto host = server.substr(0, p), port = server.substr(p + 1);
+        auto remote = resolver_.resolve(host, port);
+
+        asio::connect(socket_, remote);
+    }
+
+    void osc_enable(asio::ip::port_type port)
+    {
+        async_send("OSC SUBSCRIBE " + std::to_string(port) + "\r\n");
+    }
+    void osc_disable(asio::ip::port_type port)
+    {
+        async_send("OSC UNSUBSCRIBE " + std::to_string(port) + "\r\n");
+    }
 };
 
 }
